edb51255d94e364a69b1ca582e608aa93768ca47
a scarlett <34439046+a-scarlett@users.noreply.github.com>
2022-05-16 19:30:41 +0300

    Update 2.c

--- 5.3 Макроопределения/2.c
+++ 5.3 Макроопределения/2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <locale.h>
 
-#define W(X,Y) printf("содержимое %s = \"содержимое %s\"", #X, #Y)
+#define W(X,Y) printf("word one %s = \"word two %s\"", #X, #Y)
 
 
 int main() {
